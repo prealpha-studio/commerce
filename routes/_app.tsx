e84081a85b418b5526b4950b317dadc7ad35a727
GYO <gyo@gyo-air.local>
2025-06-08 20:30:05 -0700

    first

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>deno-gsap</title>
+        <title>alpha prototype -- 2025</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
--- routes/index.tsx
+++ routes/index.tsx
@@ -10,17 +10,28 @@ export default define.page(function Home() {
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
-          src="/logo.svg"
+          src="/main-logo.svg"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="alpha prototype"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <img
+          class="my-6"
+          src="/alpha.svg"
+          width="128"
+          height="128"
+          alt="alpha prototype"
+        />
+        <img
+          class="my-6"
+          src="/prototype.svg"
+          width="128"
+          height="128"
+          alt="alpha prototype"
+        />
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          2025
         </p>
-        <Counter count={count} />
       </div>
     </div>
   );
